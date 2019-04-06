6bc4b87c9c985fe445c097acf6c82ab3c2c31b19
Steve2312 <34347838+Steve2312@users.noreply.github.com>
2019-04-06 10:56:05 +0200

    Add files via upload

---
+++ script/kawaii.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+window.onscroll = function () { myFunction() };
+
+    var navbar = document.getElementById("navbar");
+    var navbarmargin = document.getElementById("navbarmargin");
+    var sticky = navbar.offsetTop;
+    var x = window.matchMedia("(max-width: 974px)")
+
+    console.log(window.pageYOffset);
+
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            navbar.classList.add("sticky")
+        } 
+        if (!window.pageYOffset > sticky) {
+            navbar.classList.remove("sticky");
+        }
+
+        var firstrow = document.getElementById("firstrow");
+        var firstrow2 = document.getElementById("firstrow2");
+        var firstrowtitle = document.getElementById("firstrowtitle");
+        if (window.pageYOffset > 150) {
+            firstrow.style.display = 'block';
+            firstrow2.style.display = 'block';
+            firstrowtitle.style.display = 'block';
+        } 
+    }
+});
+
+function submit() {
+    var tarckAPI = "https://t.17track.net#nums=";
+    var input = document.getElementById('tracking_number').value
+
+    if (!input) {
+        return alert("Please provide a tracking number!");
+    }
+
+    if (input) {
+        return window.open(tarckAPI + input); 
+    }
+}
