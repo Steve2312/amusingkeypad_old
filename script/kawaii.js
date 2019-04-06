510e221fea1c4e387f87d4814880dafc7f7d224d
Steve2312 <34347838+Steve2312@users.noreply.github.com>
2019-04-06 16:29:35 +0200

    21

--- script/kawaii.js
+++ script/kawaii.js
@@ -39,3 +39,12 @@ function submit() {
         return window.open(tarckAPI + input); 
     }
 }
+
+function v3download() {
+    var download = "https://gitee.com/Antecer/OsuKeyboard/raw/master/OsuKeyboard/bin/Release/OsuKeyboard.exe";
+    window.open(download);
+}
+function prodownload() {
+    var download = "https://github.com/Antecer/AmusingKeypad/releases/latest";
+    window.open(download);
+}
