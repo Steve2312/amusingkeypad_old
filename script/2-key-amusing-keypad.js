4c270ac945575d4bb228a42d0f1998f8b21c6bca
Steve2312 <34347838+Steve2312@users.noreply.github.com>
2019-06-14 15:13:49 +0200

    test

--- script/2-key-amusing-keypad.js
+++ script/2-key-amusing-keypad.js
@@ -1,8 +1,35 @@
 $(document).ready(function () {
 
     document.getElementById("addtocart").addEventListener('click', function () {
-        window.open('https://www.paypal.com/cgi-bin/webscr', 'paypal', 'width=400,height=700');
-        document.getElementById("2kbuyform").submit();
+
+        var p = document.querySelector('.packagetype:checked');
+        var c = document.querySelector('.color:checked');
+
+        if (!p) {
+            document.getElementById("packagetypetext").style.color = "red";
+        }
+
+        if (p) {
+            document.getElementById("packagetypetext").style.color = "white";
+        }
+
+        if (!c) {
+            document.getElementById("colortext").style.color = "red";
+        }
+
+        if (c) {
+            document.getElementById("colortext").style.color = "white";
+        }
+
+        if (p || c) {
+            var l = window.localStorage || 0;
+
+            var localCountry = l.getItem("country");
+
+            var url2 = window.location.pathname;
+
+            window.location.replace(url2 + `/checkout/` + `?country=${localCountry}` + `&packagetype=${p.value}&color=${c.value}`);
+        }
     });
 
 });
